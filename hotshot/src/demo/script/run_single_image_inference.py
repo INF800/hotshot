ee9c199a7abf516c4a50a09c9c918e92b46d8300
Asapanna Rakesh <rakeshark22@gmail.com>
2021-03-17 13:41:38 +0530

    interface defined

---
+++ hotshot/src/demo/script/run_single_image_inference.py
@@ -0,0 +1,21 @@
+from hotshot.xp import utils as xputils
+from hotshot.xp import Predict
+
+SAVED_MODEL_PATH = ...
+SAVE_ALL_PRED_IMGS_PATH = None
+INFERENCE_CONFIG = dict(
+    thresh=0.5,
+    margin=1.,
+    save_preds=SAVE_ALL_PRED_IMGS_PATH
+)
+
+
+predictor = Predict(
+    model_path=SAVED_MODEL_PATH
+)
+
+
+predictor.predict(
+    im_or_im_path=...
+    config=INFERENCE_CONFIG
+)
